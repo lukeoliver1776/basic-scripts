c42c186458518ca2b30a561bdf3a0acf87e261ac
Luke Oliver <lukeoliver@Lukes-MacBook-Pro.local>
2025-09-11 17:41:20 -0400

    Initial commit + docs

---
+++ bulkfilemover.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+#Bulk file mover utility
+#Moves files from one directory to another based on user input
+#WARNING this will move ALL files with the inputted file extension from the original directory to the inputted directory.
+#This Script is intended for bulk moving of files, not individual files.
+#Created with the assistance of Copilot
+
+echo "Welcome to BFMU - Bulk File Mover Utility"
+echo "This utility moves files from one directory to another."
+echo "Please enter the source directory path:"
